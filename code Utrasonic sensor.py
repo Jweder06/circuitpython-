f57d1a874357c05b63cfb3da89ef5fbd062f28f1
jweder06 <jweder06@charlottesvilleschools.org>
2022-09-20 10:44:09 -0400

    finshed Ultrasonic

--- code Utrasonic sensor.py
+++ code Utrasonic sensor.py
@@ -1,22 +1,28 @@
+
 import board
 import adafruit_hcsr04
 import neopixel
 import time
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
-Kaz = neopixel.NeoPixel(board.NEOPIXEL, 1)#connecting the neopixel on the board to the code
-Kaz.brightness = .3  #setting the brightness of the light, from 0-1 brightness
-
+import simpleio
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D6, echo_pin=board.D5)
+Dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+Dot.brightness = .3  
 while True:
     try:
         cm = sonar.distance
-        print((sonar.distance))
+        print((cm))
+        x= simpleio.map_range(cm,5,20,255,0)
+        y= simpleio.map_range(cm,20,35,0,255)
+        z= simpleio.map_range(cm,5,20,0,255)
         time.sleep(0.5)
-        if cm < 10:
-            Kaz.fill((0, 0, 255))#setting the color with RGB values
-        if cm > 10 and cm < 20:
-            Kaz.fill((255, 0, 0))#setting the color with RGB values
+        if cm < 5:
+            Dot.fill((255, 0, 0))
+        elif cm < 20:
+            Dot.fill((x, 0, z))
         else:
-            Kaz.fill((0, 255, 0))#setting the color with RGB values
+            z= simpleio.map_range(cm,20,35,255,0)
+            Dot.fill((0, y, z))
     except RuntimeError:
         print("Retrying!")
-    time.sleep(0.1)
+        time.sleep(0.1)
+    #simpleio.map_range(x, in_min, in_max, out_min, out_max)
